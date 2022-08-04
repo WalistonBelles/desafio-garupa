7bd39db67607691764562b6e1504642517b680d3
“WalistonBelles” <walistonbelles1@gmail.com>
2022-08-04 16:15:01 -0300

    fix: ajuste no retorno do endpoint

--- user_api/app/Controllers/Http/BeerController.ts
+++ user_api/app/Controllers/Http/BeerController.ts
@@ -1,6 +1,5 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import HttpBody from 'App/Models/Transfer/HttpBody'
-import HttpHeader from 'App/Models/Transfer/HttpHeader'
 import BeerService from 'App/Services/BeerService'
 export default class UserController {
 
@@ -8,13 +7,9 @@ export default class UserController {
 
   public async search(ctx: HttpContextContract) {
     const result = await this.beerService.search(ctx.request.param('page'), ctx.request.param('per_page'));
-
-    const headers: HttpHeader[] = [
-      { key: 'Content-type', value: 'application/json' }
-    ];
-
+    
     const body: HttpBody = { code: 'SEARCH_SUCCESS', result };
 
-    ctx.response.status(200).send({body, headers });
+    ctx.response.status(200).send({ code: body.code, result: body.result.body});
   }
 }
